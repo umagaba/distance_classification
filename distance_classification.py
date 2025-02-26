095f59d311e4b14bc67d88b08ff3376f755edf0d
umagaba <gabauma@gmail.com>
2025-02-26 20:33:59 +0530

    Initial project setup

---
+++ distance_classification.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.spatial import distance
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
+# Load the image
+img = cv2.imread("plaksha_Faculty.jpg")
+
+# Convert the image to grayscale
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Load the Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontal
